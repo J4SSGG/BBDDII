7c560498faecbbfb2060d3c4777614a63cc9a4a6
J4SSGG <jasg.sotogutierrez@gmail.com>
2018-10-30 12:58:01 -0600

    Modificar repositorio para alojar solo archivos SQL

---
+++ Abraham/Inserts.sql
@@ -0,0 +1,31 @@
+USE PRegistroAcademico
+GO
+INSERT INTO Horario VALUES
+	(1, '07:00:00', '08:30:00'),
+	(2, '08:40:00', '10:10:00'),
+	(3, '10:40:00', '12:10:00'),
+	(4, '12:20:00', '13:50:00'),
+	(5, '14:20:00', '15:50:00'),
+	(6, '15:50:00', '17:20:00'),
+	(7, '17:30:00', '19:00:00'),
+	(8, '19:30:00', '21:00:00')
+GO
+INSERT INTO Ciclo VALUES
+	(1, 'Primer Ciclo 2018'),
+	(2, 'Interciclo 2018'),
+	(3, 'Segundo Ciclo 2018'),
+	(4, 'Primer Ciclo 2019'),
+	(5, 'Inteciclo 2019'),
+	(6, 'Segundo Ciclo 2019')
+GO
+INSERT INTO Facultad VALUES
+	(1, 'Humanidades'),
+	(2, 'Ingeniería'),
+	(3, 'Arquitectura y Diseño'),
+	(4, 'Ciencias Económicas y Empresariales'),
+	(5, 'Ciencas Políticas y Sociales'),
+	(6, 'Ciencias Ambietales y Agrícolas'),
+	(7, 'Ciencias de la Salud'),
+	(8, 'Ciencias Jurídicas y Sociales'),
+	(9, 'Teología')
+GO
