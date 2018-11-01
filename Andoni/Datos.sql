5b5f74bbdfbd24c68be459c970e97550df4517b9
J4SSGG <jasg.sotogutierrez@gmail.com>
2018-11-01 11:09:58 -0600

    Corregido el query

--- Andoni/Datos.sql
+++ Andoni/Datos.sql
@@ -1,13 +1,13 @@
-INSERT INTO Carrera 
-(10, 1 , "Licenciatura en Educación Inicial y Preprimaria"),
-(11, 1 , "Licenciatura en Letras y Filosofía"),
-(12, 1 , "Licenciatura en Ciencias de la Comunicación"),
-(13, 1 , "Licenciatura en Psicología Industrial/Organizacional"),
-(14, 1 , "Licenciatura en Psicología Clínica"),
-(15, 1 , "Licenciatura en Educación y Aprendizaje"),
-(50, 5 , "Licenciatura en Ciencia Política"),
-(51, 5 , "Licenciatura en Relaciones Internacionales"),
-(70, 7 , "Licenciatura en Medicina"),
-(71, 7 , "Licenciatura en Nutrición"),
-(72, 7 , "Licenciatura en Terapia Respiratoria"),
-(90, 9 , "Licenciatura en Teología")
+INSERT INTO Carrera VALUES 
+(10, 1 , 'Licenciatura en Educación Inicial y Preprimaria'),
+(11, 1 , 'Licenciatura en Letras y Filosofía'),
+(12, 1 , 'Licenciatura en Ciencias de la Comunicación'),
+(13, 1 , 'Licenciatura en Psicología Industrial/Organizacional'),
+(14, 1 , 'Licenciatura en Psicología Clínica'),
+(15, 1 , 'Licenciatura en Educación y Aprendizaje'),
+(50, 5 , 'Licenciatura en Ciencia Política'),
+(51, 5 , 'Licenciatura en Relaciones Internacionales'),
+(70, 7 , 'Licenciatura en Medicina'),
+(71, 7 , 'Licenciatura en Nutrición'),
+(72, 7 , 'Licenciatura en Terapia Respiratoria'),
+(90, 9 , 'Licenciatura en Teología')
